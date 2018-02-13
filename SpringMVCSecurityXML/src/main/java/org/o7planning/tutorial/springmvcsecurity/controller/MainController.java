42a483a12fd5446d1777b405dd32b062028ae755
yerriswamykonanki <yerriswamykonanki@gmail.com>
2018-02-13 15:03:41 +0530

    updated welcome message

--- SpringMVCSecurityXML/src/main/java/org/o7planning/tutorial/springmvcsecurity/controller/MainController.java
+++ SpringMVCSecurityXML/src/main/java/org/o7planning/tutorial/springmvcsecurity/controller/MainController.java
@@ -13,7 +13,7 @@ public class MainController {
     @RequestMapping(value = { "/", "/welcome" }, method = RequestMethod.GET)
     public String welcomePage(Model model) {
         model.addAttribute("title", "Welcome");
-        model.addAttribute("message", "This is welcome page!");
+        model.addAttribute("message", "This is the updated welcome page!");
         return "welcomePage";
     }
  
